2b162535d0d27b5715f53429320ae5262f243ef7
Namkory <nguyenhoainamkory@gmail.com>
2024-02-20 16:36:59 +0700

    update2

--- src/main/java/kory/spring/com/bekoryfurniture/controller/HomeController.java
+++ src/main/java/kory/spring/com/bekoryfurniture/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class HomeController {
 
     @GetMapping()
